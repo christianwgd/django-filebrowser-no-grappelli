6477d79e386d3c91c274c9464c0c927a02982e8f
Christian Wiegand <christianwgd@users.noreply.github.com>
2022-03-02 20:46:28 +0100

    Fix upload to pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='django-filebrowser-no-grappelli2',
-    version='4.0.1',
+    version='4.0.2',
     description='Media-Management no Grappelli',
     long_description=read('README.rst'),
     url='https://github.com/christianwgd/django-filebrowser-no-grappelli',
